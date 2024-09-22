6a840b1485f85764bb09c82ce14fbdf3619ae0b9
Muhamad Irfan Afriyansyah <91465503+Irfandroid@users.noreply.github.com>
2024-09-22 18:15:25 +0000

    First Learn

---
+++ variable.js
@@ -0,0 +1,28 @@
+// var, let, const
+
+var global='Hello, my name is Irfan';
+console.log(global);
+
+var nama = 'Muhamad Irfan Afriyansyah';
+    nama = 'M Irfan A';
+console.log('Nama Saya adalah',nama);
+
+const umur = 23;
+    //umur = 24
+
+function printDataNama(nama='John', umur= 17){
+
+}
+printDataNama();    
+//snake case    
+let dataSaya = 'Nama saya adalah ${nama} dan umur saya adalah ${umur}'; 
+console.log('Nama Saya Adalah', nama, 'Umur saya', umur);    
+
+//Object -> Property -> Nested Variable
+''
+const data_Saya = {
+    nama: 'Muhamad Irfan',
+    umur: 23,
+    printDataSaya: (nama)
+}
+
